4037f1f06e37cdb901a065edb1d840a867291d29
AdilZouitine <adilzouitinegm@gmail.com>
2023-07-04 17:36:06 +0200

    add entrypoint

---
+++ rrlcs/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from gymnasium.envs.registration import register
+
+from . import envs, wrapper
+
+from ._interface import (
+    ModifiedParams,
+    ModifiedParamsEnv,
+)
+
+
+def register_robotics_envs():
+    pass
