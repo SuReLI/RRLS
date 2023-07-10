bd96b21315fef7354d9d9cf390a7625ba7cb7633
AdilZouitine <adilzouitinegm@gmail.com>
2023-07-10 16:58:22 +0200

    fix name

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
+from __future__ import annotations
+
 import pathlib
 
 from setuptools import setup
 
-
 CWD = pathlib.Path(__file__).absolute().parent
-
 
 
 def get_version():
     """Gets the rrlcs version."""
     path = CWD / "rrlcs" / "__version__.py"
-    about = {} # type: ignore
+    about = {}  # type: ignore
     with open(path) as f:
         exec(f.read(), about)
     return about["__version__"]
 
-setup(name="mo-gymnasium", version=get_version(), long_description=open("README.md").read())
+
+setup(
+    name="rrlcs",
+    version=get_version(),
+    long_description=open("README.md").read(),
+)
