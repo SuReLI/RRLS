619367d5ac678f68a0deecf5f4380c497fba6d2b
AdilZouitine <adilzouitinegm@gmail.com>
2023-07-17 16:30:57 +0200

    add test domain randomization

---
+++ test/test_domain_randomization.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import gymnasium as gym
+import pytest
+
+dr_envs = []
+envs = gym.envs.registry  # pyright: ignore
+for env in envs:
+    if ("robust" in env) and ("domain" in env):
+        dr_envs.append(gym.make(env))
+
+
+@pytest.mark.parametrize("env", dr_envs)
+def test_run_multiple_episode(env):
+    done = False
+    truncated = False
+    _, _ = env.reset()
+    while not done and not truncated:
+        action = env.action_space.sample()
+        _, _, done, truncated, _ = env.step(action)
