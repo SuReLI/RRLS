a8bbb3eb667abad55a8f5930933f7cbcb5c58fc4
AdilZouitine <adilzouitinegm@gmail.com>
2023-07-17 16:30:45 +0200

    add test ant

---
+++ test/test_ant.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import gymnasium as gym
+import pytest
+
+env = gym.make("robust-ant")
+
+
+@pytest.mark.parametrize("ant_env", [env])
+def test_ant_change_params(ant_env):
+    desired_torsomass = 3.0
+    desired_frontleftlegmass = 4.0
+    desired_frontrightlegmass = 5.0
+
+    env.set_params(
+        torsomass=desired_torsomass,
+        frontleftlegmass=desired_frontleftlegmass,
+        frontrightlegmass=desired_frontrightlegmass,
+    )
+    env.change_params()
+
+    assert env.model.body_mass[1] == desired_torsomass
+    assert env.model.body_mass[2] == desired_frontleftlegmass
+    assert env.model.body_mass[4] == desired_frontrightlegmass
+
+    assert env.get_params() == {
+        "torsomass": desired_torsomass,
+        "frontleftlegmass": desired_frontleftlegmass,
+        "frontrightlegmass": desired_frontrightlegmass,
+    }
