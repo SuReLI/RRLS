fcef6310089b5b2e617e2d966878f41378bf2339
AdilZouitine <adilzouitinegm@gmail.com>
2023-07-17 16:31:56 +0200

    add test walker

---
+++ test/test_walker.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import gymnasium as gym
+import numpy as np
+import pytest
+
+env = gym.make("robust-walker")
+
+
+@pytest.mark.parametrize("walker_env", [env])
+def test_walker_change_params(walker_env):
+    desired_worldfriction = 3.0
+    desired_torsomass = 4.0
+    desired_thighmass = 5.0
+
+    env.set_params(
+        worldfriction=desired_worldfriction,
+        torsomass=desired_torsomass,
+        thighmass=desired_thighmass,
+    )
+    env.change_params()
+
+    assert np.array_equal(
+        env.model.geom_friction[:, 0],
+        np.array([3.0, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 1.9]),
+    )
+    assert env.model.body_mass[1] == desired_torsomass
+    assert env.model.body_mass[2] == desired_thighmass
+
+    assert env.get_params() == {
+        "worldfriction": desired_worldfriction,
+        "torsomass": desired_torsomass,
+        "thighmass": desired_thighmass,
+    }
