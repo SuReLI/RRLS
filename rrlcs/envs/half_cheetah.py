4036547125b9356191ff3a3adccc31733daee62d
AdilZouitine <adilzouitinegm@gmail.com>
2023-07-19 15:27:13 +0200

    remove parameters bounds inside the class and add more parameters

--- rrlcs/envs/half_cheetah.py
+++ rrlcs/envs/half_cheetah.py
@@ -21,34 +21,32 @@ class HalfCheetahParamsBound(Enum):
 
 
 class RobustHalfCheetah(HalfCheetahEnv):
-    ONE_DIM_PARAMS_BOUND_3 = {
-        "worldfriction": [0.1, 3.0],
-    }
-    ONE_DIM_PARAMS_BOUND_4 = {
-        "worldfriction": [0.1, 4.0],
-    }
-
-    TWO_DIM_PARAMS_BOUND_4_7 = {
-        "worldfriction": [0.1, 4.0],
-        "torsomass": [0.1, 7.0],
-    }
-    THREE_DIM_PARAMS_BOUND_3_7_4 = {
-        "worldfriction": [0.1, 4.0],
-        "torsomass": [0.1, 7.0],
-        "backthighmass": [0.1, 3.0],
-    }
-
     def __init__(
         self,
         worldfriction: float | None = None,
         torsomass: float | None = None,
         backthighmass: float | None = None,
+        backshinmass: float | None = None,
+        backfootmass: float | None = None,
+        forwardthighmass: float | None = None,
+        forwardshinmass: float | None = None,
+        forwardfootmass: float | None = None,
     ):
         self.worldfriction = worldfriction
         self.torsomass = torsomass
         self.backthighmass = backthighmass
         super().__init__()
 
+        self.set_params(
+            worldfriction=worldfriction,
+            torsomass=torsomass,
+            backthighmass=backthighmass,
+            backshinmass=backshinmass,
+            backfootmass=backfootmass,
+            forwardthighmass=forwardthighmass,
+            forwardshinmass=forwardshinmass,
+            forwardfootmass=forwardfootmass,
+        )
         self.change_params()
 
     def set_params(
@@ -56,16 +54,31 @@ class RobustHalfCheetah(HalfCheetahEnv):
         worldfriction: float | None = None,
         torsomass: float | None = None,
         backthighmass: float | None = None,
+        backshinmass: float | None = None,
+        backfootmass: float | None = None,
+        forwardthighmass: float | None = None,
+        forwardshinmass: float | None = None,
+        forwardfootmass: float | None = None,
     ):
         self.worldfriction = worldfriction
         self.torsomass = torsomass
         self.backthighmass = backthighmass
+        self.backshinmass = backshinmass
+        self.backfootmass = backfootmass
+        self.forwardthighmass = forwardthighmass
+        self.forwardshinmass = forwardshinmass
+        self.forwardfootmass = forwardfootmass
 
     def get_params(self):
         return {
             "worldfriction": self.worldfriction,
             "torsomass": self.torsomass,
             "backthighmass": self.backthighmass,
+            "backshinmass": self.backshinmass,
+            "backfootmass": self.backfootmass,
+            "forwardthighmass": self.forwardthighmass,
+            "forwardshinmass": self.forwardshinmass,
+            "forwardfootmass": self.forwardfootmass,
         }
 
     def reset(self, *, seed: int | None = None, options: dict | None = None):
@@ -85,3 +98,13 @@ class RobustHalfCheetah(HalfCheetahEnv):
             self.model.body_mass[1] = self.torsomass
         if self.backthighmass is not None:
             self.model.body_mass[2] = self.backthighmass
+        if self.backshinmass is not None:
+            self.model.body_mass[3] = self.backshinmass
+        if self.backfootmass is not None:
+            self.model.body_mass[4] = self.backfootmass
+        if self.forwardthighmass is not None:
+            self.model.body_mass[5] = self.forwardthighmass
+        if self.forwardshinmass is not None:
+            self.model.body_mass[6] = self.forwardshinmass
+        if self.forwardfootmass is not None:
+            self.model.body_mass[7] = self.forwardfootmass
