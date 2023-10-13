e99ca7c5facd2e827144bd150a3fd1fce10d52e4
AdilZouitine <adilzouitinegm@gmail.com>
2023-10-13 21:23:40 +0200

    rename package

--- rrlcs/__version__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-VERSION = (0, 0, 1)
-
-__version__ = ".".join(map(str, VERSION))  # noqa: F401
--- rrlcs/wrapper/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from .adversarial import DynamicAdversarial
-from .domain_randomization import DomainRandomization
-
-__all__ = ["DynamicAdversarial", "DomainRandomization"]
